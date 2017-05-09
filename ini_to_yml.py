c9f03b3b037e71079e942dd88ad648c00baa2142
Rob Crawford <robertcrawford7755@gmail.com>
2017-05-09 21:50:05 +0100

    added initial files

---
+++ ini_to_yml.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import yaml
+import argparse
+
+def process_ini(inifile,ymlfile):
+    config = ConfigParser.ConfigParser()
+    config.read(inifile)
+   
+    datamap = {} 
+    for section in config.sections():
+        datamap[section] = {}
+        for name,value in config.items(section):
+            datamap[section].update({name:value})
+
+    with open(ymlfile,"w") as ymlfile:
+        yaml.dump(datamap,ymlfile,default_flow_style=False)     
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert a basic ini file to yml")
+    parser.add_argument('--in',action="store",dest="ini",required=True,help="Input ini file")
+    parser.add_argument('--out',action="store",dest="yml",required=True,help="Output yml file")
+    args = vars(parser.parse_args())
+    inifile = args['ini']
+    ymlfile = args['yml']
+
+    process_ini(inifile,ymlfile) 
+
+if __name__ == '__main__':
+    main()
